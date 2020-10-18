871c463f96f86fcaef65f642f61c4b7d67350a80
Chrysanthemum Munatsi <christubbie@gmail.com>
2020-10-18 05:44:41 +0200

    Create intro.rb

---
+++ intro.rb
@@ -0,0 +1,38 @@
+## Problem 1
+# Write a method, count_vowels(word), that takes in a string word and returns the
+number of vowels in the word. Vowels are the letters a, e, i, o, u.
+
+def count_vowels(word)
+  
+  count = 0
+  i = 0
+  
+  while i < word.length
+    char = word[i]
+    if char == "a" || char == "e" || char == "i" || char == "o" || char == "u" 
+        count += 1
+    end 
+  	i += 1
+  
+  end
+  
+	return count
+end
+
+
+## Problem 2
+# Write a method sum_nums(max) that takes in a number max and returns the sum of
+all numbers from 1 up to and including max.
+
+def sum_nums(max)
+  
+  sum = 0
+  i = 1
+  
+  while i <= max
+    sum += i
+    i += 1
+  end
+  return sum
+
+end
