283f76da43a14b84bc911827de9a680ea76d22ac
Chrysanthemum Munatsi <christubbie@gmail.com>
2020-10-19 03:56:14 +0200

    Update intro.rb

--- intro.rb
+++ intro.rb
@@ -50,9 +50,23 @@ def factorial(num)
     i += 1
   end
   return mult
-
-
+end
 end
 
+## Problem 4
+# Create a function that reverses words
+def reverse(word)
 
+ reversed = ""
+ i = 0
+  
+	while i < word.length
+      char = word[i]
+      reversed = char + reversed
+      i += 1  
+    end
+  
+  return reversed 
+      
 end
+
